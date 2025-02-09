d5fd2692083d4340b16b8009359f9acfadcacd4a
Troy74x <hpg350@gmail.com>
2025-02-10 01:16:24 +0200

    Updated .gitignore , removed ignored files , added Repository Class

---
+++ RestaurantOrderingSystem/Models/QueryOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace RestaurantOrderingSystem.Models
+{
+    public class QueryOptions<T> where T : class
+    {
+        //This is an expression (a LINQ function) that defines how to sort the query results.
+        public Expression<Func<T, Object>> OrderBy { get; set; } = null!;
+
+        //This allows defining a filter condition dynamically.
+        public Expression<Func<T, bool>> Where { get; set; } = null!;
+
+
+
+
+        private string[] includes = Array.Empty<string>();
+
+        public string Includes
+        {
+            set => includes = value.Replace(" ", "").Split(',');
+        }
+
+        public string[] GetIncludes() => includes;
+
+
+
+
+        public bool HasWhere => Where != null; //Returns true if a filter condition is set.
+        public bool HasOrderBy => OrderBy != null; //HasOrderBy: Returns true if a sorting order is set.
+    }
+}
