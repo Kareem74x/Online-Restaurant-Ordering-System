e8af9ae697a2f274fe6cfe9486a11a636817e23f
Troy74x <hpg350@gmail.com>
2025-02-15 23:57:30 +0200

    Added Add item to order and navbar cart

--- RestaurantOrderingSystem/Controllers/OrderController.cs
+++ RestaurantOrderingSystem/Controllers/OrderController.cs
@@ -36,5 +36,53 @@ namespace RestaurantOrderingSystem.Controllers
 
             return View(model);
         }
+
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddItem(int prodId,int ProdQty)
+        {
+            var product = await _context.Products.FindAsync(prodId);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Retrieve or create new OrderViewModel from session or other state mangaement
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel") ?? new OrderViewModel
+            {
+                OrderItems = new List<OrderItemViewModel>(),
+                Products = await _products.GetAllAsync()
+            };
+
+            // Check if the product is already in the order, update quantity
+            var existingItem = model.OrderItems.FirstOrDefault(oi => oi.ProductId == prodId);
+
+            // if the product already exist , Update the quantity
+            if(existingItem !=null)
+            {
+                existingItem.Quantity += ProdQty;
+            }
+            else
+            {
+                model.OrderItems.Add(new OrderItemViewModel
+                {
+                    ProductId = product.ProductId,
+                    Price = product.Price,
+                    Quantity = ProdQty,
+                    ProductName = product.Name
+                });
+            }
+
+
+            model.TotalAmount = model.OrderItems.Sum(oi => oi.Quantity * oi.Price);
+
+            // Save updated OrderViewModel to session
+            HttpContext.Session.Set("OrderViewModel", model);
+
+            // Redirect back to Create to show updated order items
+            return RedirectToAction("Create",model);
+        }
     }
 }
